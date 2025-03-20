7d7c1de7a590e0161192530cc0cf11cfd616ba6b
HuriaTahiry <126624992+HuriaTahiry@users.noreply.github.com>
2025-03-20 11:15:55 -0700

    Add files via upload

---
+++ pig_game.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Huria Tahiry <-- Put your name here.
+"""
+import random
+
+"""
+function: take_turn()
+This function lets a player take a turn in the 1-die game.
+
+As shown in the video, a player turn requires the player to
+make at least one roll of the die.  If the player rolls a 1,
+then the turn ends with no points earned.  If the player rolls
+anything else, then the player can choose to keep rolling or
+to stop.  When the player chooses to stop, the player earns the
+sum of all rolls this turn.
+
+Use a console input of r to indicate to keep rolling and use a
+console input of anything else to indicate to stop rolling.
+
+Ensure to print the accumulated turn score each time the player
+rolls any value other than 1.
+
+Return the number of points earned on the turn.
+
+Note: the initial code given would be just return 0
+"""
+
+
+def take_turn():
+    return 0  # replace this with your code
+
+
+"""
+function: play_one_turn_pig()
+- This function is already completed.  Inspect it to help you
+  implement the next function.
+- This function lets a player play a very short Pig game, which
+  ends after exactly 1 turn.
+
+Ensure to print "1-Turn Pig Time!" before starting the game
+and ensure to print the score earned by the player at game's end.
+
+Note: we might even give this to them to make it easier to test
+the take_turn_one_die() implementation.
+"""
+
+
+def play_one_turn_pig():
+    goal_score = 0
+    while True:
+        roll = random.randint(1, 6)
+        if roll ==1:
+            goal_score = 0
+            print("You rolled a 1. Your turn is over.")
+        else:
+            goal_score += roll
+            print(f"You rolled  {roll}.")
+            print(f'Your score is {goal_score}.')
+            choice = input("(r)oll again? ")
+            if choice.lower() != "r":
+                break
+    score = take_turn()
+    print('You scored', goal_score)
+
+
+"""
+function: play_solitaire_pig()
+This function lets a player play a solitaire Pig game.
+
+Prompt the player for a target score, then let the player
+take turns until they reach or exceed that score.
+
+Ensure to print "Solitaire Pig Time!" before starting the game,
+print the player's score and turn number after each turn, and
+print the final score when the game ends.  Refer to the video
+for guidance or example.
+"""
+
+
+def play_solitaire_pig():
+    target_score = int(input('Target Score?'))
+    target = 0
+    while True:
+        roll = random.randint(1, 6)
+        if roll == 1:
+            target = 0
+            print('oink')
+        else:
+            target += roll
+            if target >= target_score:
+                print('you win!')
+                break
+            print(f"You rolled  {roll}.")
+            print(f'Your score is {target}.')
+            choice = input("(r)oll again? ")
+            if choice.lower() != "r":
+                break
+    print('Solitaire Pig Time!')
+
+
+"""
+function: play_heads_up_pig()
+This function lets 2 players play a heads-up Pig game.
+
+This should proceed the same way as solitaire except alternating
+between player 1 and player 2.  If player 1 exceeds the target score,
+then player 2 gets one more turn before the game ends.  If player 2
+exceeds the target score, then the game ends right away.
+
+Ensure to print "Heads-up Pig Time!" before starting the game,
+print which player's turn it is before starting the turn,
+print each player's score and the round number at the end of
+the round, and print both scores at game end.  Refer to the video
+for guidance or example.
+"""
+
+
+def play_heads_up_pig():
+    import random
+    # Function for player 1
+    def play1(score1):
+        print("Player1:")
+        roll = random.randint(1, 6)
+        print(f" You rolled a {roll}")
+        if roll == 1:
+            print("Oink")
+            score1 = 0
+            print(f"Player 1's score: {score1}")
+            print("x = 0")
+            return 0
+        else:
+            print(f"Player 1's score: {score1 + roll}")
+            return roll
+
+    # Function for player 2
+    def play2(score2):
+        print("Player2:")
+        roll = random.randint(1, 6)
+        print(f" You rolled a {roll}")
+        if roll == 1:
+            print("Oink")
+            score2 = 0
+            print(f"Player 2's score: {score2}")
+            return 0
+        else:
+            print(f"Player 2's score: {score2 + roll}")
+            return roll
+
+    # code
+    target_score = int(input("Input the target score."))
+    score1 = 0
+    score2 = 0
+    while score1 < target_score and score2 < target_score:
+        x = play1(score1)
+        if x == 0:
+            score1 = 0
+        else:
+            score1 = score1 + x
+            choice1 = input("(r)oll again?")
+            while choice1 == "r" and score1 < target_score:
+                x1 = play1(score1)
+                score1 = score1 + x1
+                choice1 = input("(r)oll again?")
+                if score1 >= target_score:
+                    print(f"Player 1 won. Your score is {score1}.")
+            break;
+
+        y = play2(score2)
+        if y == 0:
+            score2 = 0
+        else:
+            score2 = score2 + y
+            choice2 = input("(r)oll again?")
+            while choice2 == "r" and score2 < target_score:
+                y1 = play2(score2)
+                score2 = score2 + y1
+                choice2 = input("(r)oll again?")
+                if score2 >= target_score:
+                    print(f"Player 2 won. Your score is {score2}.")
+            break;
+
+    # if score1 >= target_score:
+    #   print(f"Player 1 won. Your score is {score1}.")
+    # else:
+    #   print(f"Player 2 won. Your score is {score2}.")
+
+    # while True:
+    #     print('new turn: Player2')
+    #     roll = random.randint(1, 6)
+    #     if roll == 1:
+    #         print('oink')
+    #         break
+    #     scores2 += roll
+    #     if scores2 >= target_score:
+    #         print('player2 win!')
+    #         break
+    #     print(f"You rolled  {roll}.")
+    #     print(f'Your score is {scores2}.')
+    #     choice = input("(r)oll again? ")
+    #     if choice.lower() != "r":
+    #         break
+    # roll2 = random.randint(1, 6)
+    # if roll2 == 1:
+    #     print('oink')
+    #     break
+    # else:
+    #     scores2 += roll2
+    #     scores2 >= target_score
+    #     print('player2 win!')
+    #     break
+    #     print(f"You rolled  {roll2}.")
+    #     print(f'Your score is {scores2}.')
+    #     choice = input("(r)oll again? ")
+    #     if choice.lower() != "r":
+    #         break
+
+    print('Heads-up Pig Time!')
+
+
+''' Change no code below here '''
+
+
+
+
+''' Here be dragons '''
+
+
+
+
+''' Don't change this '''
+
+
+def start():
+    games = {
+        't': ('1-Turn Pig', play_one_turn_pig),
+        's': ('Solitaire Pig', play_solitaire_pig),
+        'h': ('Heads-up Pig', play_heads_up_pig),
+    }
+    print('Menu\n----')
+    for (key, game) in games.items():
+        print(key, ": ", game[0], sep='')
+    choice = input('\nGame choice: ')
+    if choice in games:
+        games[choice][1]()
+    else:
+        print('Sorry, that is not a game choice')
+
+
+if __name__ == '__main__':
+    start()
